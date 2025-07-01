c16f929e897b35346fdfa88e02c09d345f1f726a
Melanie Clark <melaniefclark@gmail.com>
2025-07-01 23:37:02 +0100

    test_appt_checker.py initial notes added

---
+++ tests/test_appt_checker.py
@@ -0,0 +1,10 @@
+"""
+Unit Tests:
+  
+  wrong username
+  wrong password
+  
+  missing username, password, e-mail to, from, email password
+  
+  No appointments in either set of 3 week appts
+"""
