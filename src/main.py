6c88ae2880a76144ba69172215ea2f879de1634e
Melanie Clark <melaniefclark@gmail.com>
2025-10-11 23:10:47 +0100

    main.pt surgery_address added and while loop adding for continuous appointment checking

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
+import time
 import traceback
+
 from datetime import datetime
 
 from config import Config
@@ -7,6 +9,7 @@ from appointment_extractor import AppointmentExtractor
 from so_appt_nav import SystmOnlineNavigator
 from file_manager import FileManager
 from email_manager import EmailManager
+from surgery_info_extractor import SurgeryInfoExtractor
 
 
 # Main file to run the SystmOnline GP Appointment Checker
@@ -17,26 +20,43 @@ class GPAppointmentChecker:
     self.driver = self.browser.driver
     self.extractor = AppointmentExtractor(self.driver)
     self.so_appt_nav = SystmOnlineNavigator(self.driver, self.extractor)
+    self.surgery_info = SurgeryInfoExtractor(self.driver)
     self.file_manager = FileManager()
     self.email_manager = EmailManager()
-    self.no_appts = "There are currently no available appointments."
+    self.no_appts = "There are currently no available appointments at "
     
   def run(self):
     try:
       # Skip if out of hours (before 8am or after 6pm) -----------
+      # It's recommended to check for short periods frequently, or less frequent over a longer time frame
       hour = datetime.now().hour
-      if hour < 8 or hour > 18:  # changed for testing purposes
+      start_time = 8 
+      end_time = 23
+      if hour < start_time or hour > end_time:  # changed for testing purposes
         print("GP Appointment Checker has been run outside of configured hours. Update the hours in main.py or run during configured hours.")
         return
+      
+      delay_time = 60
     
       self.config.validate()
       self.so_appt_nav.login(self.file_manager, self.email_manager)
-      appt_data = self.so_appt_nav.appointment_navigation()     
-      appt_content = self.file_manager.save_appointment_data(appt_data)
-      if appt_data == 0:
-        self.email_manager.send_email("", self.no_appts, self.no_appts)
-      else:
-        self.email_manager.send_email(appt_content)
+      surgery_address = self.surgery_info.extract_address()
+
+      no_appt_text = self.no_appts + surgery_address
+      self.so_appt_nav.click_book_appointment()
+      while True:
+        appt_data = self.so_appt_nav.appointment_navigation()     
+        # error during loop -----------------------------------------
+        appt_content = self.file_manager.save_appointment_data(surgery_address, appt_data)
+        if appt_data == 0:
+          self.email_manager.send_email("", no_appt_text, no_appt_text)
+        else:
+          self.email_manager.send_email(appt_content)
+        hour = datetime.now().hour
+        time.sleep(delay_time)
+        if hour < start_time or hour > end_time: 
+          return False
+        
     except Exception as e:
       self.file_manager.log_error(f"General Exception: {str(e)}")
       traceback.print_exc()
