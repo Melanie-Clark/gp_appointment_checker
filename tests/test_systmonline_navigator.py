fb4c86a46cc8c04f5c9a72c8f62e0accbd1e39bd
Melanie Clark <melaniefclark@gmail.com>
2025-10-25 20:35:32 +0100

    test_systmonline_navigator.py appointment_navigation() successful tests added

--- tests/test_systmonline_navigator.py
+++ tests/test_systmonline_navigator.py
@@ -72,43 +72,70 @@ class TestSystmOnlineNavigator(unittest.TestCase):
     mock_button.click.assert_called_once()
     
   # ---------------APPOINTMENT NAVIGATION----------------------
+  # mock appointments for appointment_navigation() tests
+  @staticmethod
+  def _mock_appointments(date1="Saturday 25th Oct 2025", date2 = "Thursday 30th Oct 2025"):
+    return [
+      {"Date": date1, "Earliest Time": "17:00", "Latest Time": "17:00", "Location": "Test location", "Clinician": "Dr. Smith", "Session Type": "Test session"},
+      {"Date": date2, "Earliest Time": "18:05", "Latest Time": "18:05", "Location": "Test location", "Clinician": "Dr. Jones", "Session Type": "Test session"}
+    ]
+    
   # FAIL: No appointments available
   # other_appt_date_ranges from appointment_navigation() will have a separate test for the i/o, so mock in this scenario
   def test_appointment_navigation_no_appointments(self):
     self.mock_extractor.extract_appointments.return_value = []
-    
-    # Mock the helper function to avoid Selenium calls
     self.navigator.other_appointment_date_ranges = MagicMock(return_value=[])
 
     result = self.navigator.appointment_navigation()
 
-    # Assert the result is 0 since no appointments are available
+    # Assert the result as [] since no appointments are available
     self.assertEqual(result, [])
 
-    # Ensure extract_appointments() was called during test
+    # Ensure extract_appointments() and other_appointment_date_ranges() were called during test
     self.mock_extractor.extract_appointments.assert_called_once()
-    self.navigator.other_appointment_date_ranges.assert_called_once()
+    self.navigator.other_appointment_date_ranges.assert_called_once_with([])
 
   # SUCCESS: Extract initial appointments, no other appointments
-  def test_appointment_navigation_with_appointments(self):
-    mock_result = [
-      {"Date": "Saturday 25th Oct 2025", "Earliest Time": "17:00", "Latest Time": "17:00", "Location": "Test location", "Clinician": "Dr. Smith", "Session Type": "Test session"},
-      {"Date": "Thursday 30th Oct 2025", "Earliest Time": "18:05", "Latest Time": "18:05", "Location": "Test location", "Clinician": "Dr. Jones", "Session Type": "Test session"}
-    ]
-    
+  def test_appointment_navigation_with_initial_appointments_only(self):
+    mock_result = self._mock_appointments()
     self.mock_extractor.extract_appointments.return_value = mock_result
-
     self.navigator.other_appointment_date_ranges = MagicMock(return_value=mock_result)
 
     result = self.navigator.appointment_navigation()
     
-    # Assert the result is 0 since no appointments are available
     self.assertEqual(result, mock_result)
 
-    # Ensure extract_appointments() was called during test
     self.mock_extractor.extract_appointments.assert_called_once()
     self.navigator.other_appointment_date_ranges.assert_called_once_with(mock_result)
 
+  # SUCCESS: No initial appointments, other available appointments at later date
+  def test_appointment_navigation_with_no_initial_appointments_with_other_appointments(self):    
+    self.mock_extractor.extract_appointments.return_value = []
+    mock_result = self._mock_appointments()
+    self.navigator.other_appointment_date_ranges = MagicMock(return_value=mock_result)
+
+    result = self.navigator.appointment_navigation()
+    
+    self.assertEqual(result, mock_result)
+
+    self.mock_extractor.extract_appointments.assert_called_once()
+    self.navigator.other_appointment_date_ranges.assert_called_once_with([])
+
+  # SUCCESS: Extract both initial appointments and other available appointments at later date
+  def test_appointment_navigation_with_both_initial_and_other_appointments(self):
+    mock_result1 = self._mock_appointments()
+    mock_result2 = self._mock_appointments("Saturday 15th Nov 2025", "Thursday 20th Nov 2025")
+    self.mock_extractor.extract_appointments.return_value = mock_result1
+    mock_result_total = mock_result1 + mock_result2
+    self.navigator.other_appointment_date_ranges = MagicMock(return_value=mock_result_total)
+
+    result = self.navigator.appointment_navigation()
+    
+    self.assertEqual(result, mock_result_total)
+
+    self.mock_extractor.extract_appointments.assert_called_once()
+    self.navigator.other_appointment_date_ranges.assert_called_once_with(mock_result1)
+    
 
 if __name__ == "__main__":
     unittest.main()
